5e36235a4b4aea0373e732075c61994f753de16e
JobKoech610 <jobkoech610@gmail.com>
2024-02-27 20:08:49 +0300

    init

---
+++ js/footable.init.js
@@ -0,0 +1,46 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+$(document).ready(function() {
+	$(function () {
+		$('.footable').footable();
+	});
+
+// If you require any other breakpoint use bellow function	
+	//$('.footable').footable({
+		//breakpoints: {
+		//phone_p: 320,
+		//tablet_p: 768
+		//}
+	//});	
+});
+
+}
+/*
+     FILE ARCHIVED ON 12:34:47 May 07, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:54:31 Feb 26, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.12
+  exclusion.robots.policy: 0.107
+  cdx.remote: 0.106
+  esindex: 0.012
+  LoadShardBlock: 1644.75 (6)
+  PetaboxLoader3.datanode: 263.564 (7)
+  load_resource: 134.556
+  PetaboxLoader3.resolve: 82.651
+*/
